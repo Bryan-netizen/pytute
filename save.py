7a801307458d49b690af25559c08afd606102096
nooblet779 <bmkamunyu@gmail.com>
2020-10-12 00:07:19 +0300

    starting out with sublime and corey shafer

---
+++ save.py
@@ -0,0 +1,11 @@
+greeting = 'Hello'
+
+name = 'Bryan'
+
+message = 'f{greeting},{name}. Welcome!'
+
+print(message)
+
+print(dir(name)) # shows all the string methods available
+
+print(help(str.lower)) # shows help with methods
