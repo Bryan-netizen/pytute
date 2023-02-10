ab64e5cc7a7dbe2a335c6e3a34b361f65d069dcf
Sibs <68712272+Bryan-netizen@users.noreply.github.com>
2023-02-10 17:13:19 +0300

    Delete mpesa_data directory

--- mpesa_data/from tabula import read_pdf.py
+++
@@ -1,8 +0,0 @@
-from tabula import read_pdf
-import tabula
-
-#df = read_pdf("/home/sibs/Documents/mpesa_data/testdata.pdf",pages ="all")
-
-#print(data)
-tabula.convert_into("/home/sibs/Documents/mpesa_data/testdata.pdf", "testdata.csv", output_format="csv", pages="all")
-
--- mpesa_data/manelot_pdf.py
+++
@@ -1,4 +0,0 @@
-import camelot
-
-tables = camelot.read_pdf("/home/sibs/Documents/mpesa_data/testdata.pdf")
-tables
